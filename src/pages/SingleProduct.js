a6599c1978eb6ca2d3b37b5e317861e35bf388e4
kingbookk <kingbooktashkent@gmail.com>
2022-11-16 15:32:17 +0500

    router

---
+++ src/pages/SingleProduct.js
@@ -0,0 +1,14 @@
+import { Link, useParams } from 'react-router-dom';
+import products from '../data';
+const SingleProduct = () => {
+  const { productId } = useParams();
+
+  return (
+    <section className='section product'>
+      <h2>{productId}</h2>
+      <Link to='/products'>back to products</Link>
+    </section>
+  );
+};
+
+export default SingleProduct;
---
+++ src/pages/StyledNavbar.js
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+
+<nav className='navbar'>
+  <NavLink
+    to='/about'
+    style={({ isActive }) => {
+      return { color: isActive ? 'red' : 'grey' };
+    }}
+  >
+    Home
+  </NavLink>
+</nav>;
